367e06029b0837c87466d3743a1e09eee8220734
James Roy Poulter <jamesroypoulter@gmail.com>
2014-01-01 16:49:55 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toothbrush::Application.config.secret_token = '41173a57f2c625bc5e35683d29c79937dbd2a6e5768c2b70b21033afbc909ff8cfcddafd9e9b841b80deed8fb7549d51d0410523a9ddcd14c0ddb5a3ba97ab9c'
